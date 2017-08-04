7a23dc3f771763f019926722dac3427c9da214be
Smith0r <smith0r54@gmail.com>
2017-08-04 09:27:16 +0100

    Fix menumanager bug

--- admin/components/menu/views/menuitem.php
+++ admin/components/menu/views/menuitem.php
@@ -1,7 +1,7 @@
 <div class="card">
     <h2>Menuitem Manager</h2>
     <div class="action-bar">
-        <a href="index.php?component=menu&controller=menuitems"><i class="fa fa-chevron-left"></i> Back to List</a>
+        <a href="index.php?component=menu&controller=menuitems&id=<?php echo $this->model->menu_id; ?>"><i class="fa fa-chevron-left"></i> Back to List</a>
     </div>
     <form action="index.php?component=menu&controller=menuitem&task=save" method="post">
         <?php $this->model->form->display(false); ?>
