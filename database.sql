5a3637b1ebaf3cd758c6e7f48df595ca0a4c693c
Smith0r <smith0r54@gmail.com>
2017-08-04 14:35:37 +0100

    Database update

--- database.sql
+++ database.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 04, 2017 at 12:20 AM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 7.0.13
+-- Generation Time: Aug 04, 2017 at 03:35 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `burgundy`
+-- Database: `bulletin`
 --
 
 -- --------------------------------------------------------
@@ -47,7 +47,7 @@ CREATE TABLE `bgdy_articles` (
 --
 
 INSERT INTO `bgdy_articles` (`id`, `title`, `alias`, `category_id`, `content`, `published`, `publish_time`, `start_publishing`, `stop_publishing`, `author_id`, `hits`, `meta_description`, `tags`) VALUES
-(3, 'Test Article', 'test-article', 2, '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in sem sit amet est tristique hendrerit in sit amet nulla. Duis pellentesque ipsum sed mattis maximus. Suspendisse imperdiet lectus at sapien tempor, sed laoreet purus cursus. Sed molestie nunc tortor, sit amet malesuada augue pulvinar sit amet. In accumsan euismod dui quis finibus. Quisque sed nisl urna. Phasellus dui magna, fermentum vitae porta non, dignissim ut lorem. Vestibulum vel tincidunt leo. Suspendisse eget risus blandit lacus malesuada volutpat eget nec ante. Nulla mi eros, luctus quis rutrum ut, maximus id nisl. Etiam ultrices ac metus id convallis. Curabitur eu nibh non purus tincidunt efficitur. Aliquam sed fermentum velit. Maecenas consequat augue sit amet pharetra laoreet. Cras vitae nisl in libero ultricies volutpat.</p>\r\n<p>Praesent viverra, odio cursus hendrerit porttitor, est ligula accumsan turpis, vel mattis purus neque quis lorem. Aliquam erat volutpat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed felis sem, congue vitae mattis vel, scelerisque vitae ligula. Vestibulum viverra quam vel ex eleifend, quis maximus massa accumsan. Nulla facilisi. Fusce varius in nisi nec consequat. Nullam imperdiet, arcu non tempor pulvinar, ipsum felis eleifend mi, a molestie eros libero at ligula. Morbi nec lorem quis metus dignissim cursus sit amet sit amet eros. Fusce tincidunt lorem a eleifend mattis. Sed eu luctus augue. Etiam sagittis sem vitae eros tempus, a suscipit metus faucibus. Etiam commodo venenatis ipsum et pellentesque. Maecenas elementum ultrices neque, eget mollis ipsum pulvinar et. Quisque euismod dui ipsum. Suspendisse egestas mi sed orci aliquet maximus.</p>\r\n<p>Ut vel vestibulum risus, at hendrerit nisl. Aenean tristique in massa eget iaculis. Aliquam ac feugiat nunc. Morbi pellentesque venenatis nisi, quis imperdiet leo congue et. Morbi tempus laoreet erat, id fringilla dolor finibus at. Ut non diam nec arcu efficitur dignissim. Morbi ac orci nunc. Sed lobortis ligula feugiat, fringilla lorem a, sagittis odio. Quisque cursus sapien velit, vitae aliquet ipsum luctus sed. Vestibulum quis sodales orci, a lobortis lorem. Cras quis orci a libero fringilla scelerisque. Curabitur ornare lorem purus, non tincidunt velit porta eu. Morbi suscipit nulla neque, blandit cursus risus dictum eget. Curabitur viverra convallis iaculis. Nunc venenatis est et diam mattis euismod.</p>\r\n<p>Integer vehicula, elit sed rutrum ultrices, ante dolor posuere turpis, ultricies commodo nibh ante eu justo. Sed feugiat hendrerit ligula, ut tincidunt lorem iaculis quis. Donec tristique purus leo, quis blandit odio iaculis vel. Sed nec magna non velit malesuada commodo vitae vel leo. Duis ut eros vel felis sodales dignissim. Donec pellentesque vel libero non eleifend. Sed ullamcorper, magna ultricies sagittis posuere, purus nibh placerat ante, nec mollis velit quam a felis.</p>\r\n<p>Aliquam erat volutpat. Donec vitae lacus quis nisi pharetra rutrum et sed sapien. Etiam augue massa, convallis a efficitur nec, porttitor at ligula. Nullam luctus elit id sapien elementum, a placerat quam porta. Curabitur mattis purus in fermentum consectetur. Curabitur interdum nulla ligula, non pulvinar magna molestie eu. Fusce id leo a velit faucibus suscipit. Donec tincidunt leo ac vehicula volutpat. Vestibulum leo tellus, laoreet vel efficitur eget, aliquet sit amet libero. Nulla facilisi. Duis malesuada consequat cursus.</p>', 1, 1487514022, 0, 0, 5, 2, '', '');
+(3, 'Test Article', 'test-article', 2, '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in sem sit amet est tristique hendrerit in sit amet nulla. Duis pellentesque ipsum sed mattis maximus. Suspendisse imperdiet lectus at sapien tempor, sed laoreet purus cursus. Sed molestie nunc tortor, sit amet malesuada augue pulvinar sit amet. In accumsan euismod dui quis finibus. Quisque sed nisl urna. Phasellus dui magna, fermentum vitae porta non, dignissim ut lorem. Vestibulum vel tincidunt leo. Suspendisse eget risus blandit lacus malesuada volutpat eget nec ante. Nulla mi eros, luctus quis rutrum ut, maximus id nisl. Etiam ultrices ac metus id convallis. Curabitur eu nibh non purus tincidunt efficitur. Aliquam sed fermentum velit. Maecenas consequat augue sit amet pharetra laoreet. Cras vitae nisl in libero ultricies volutpat.</p>\r\n<p>Praesent viverra, odio cursus hendrerit porttitor, est ligula accumsan turpis, vel mattis purus neque quis lorem. Aliquam erat volutpat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed felis sem, congue vitae mattis vel, scelerisque vitae ligula. Vestibulum viverra quam vel ex eleifend, quis maximus massa accumsan. Nulla facilisi. Fusce varius in nisi nec consequat. Nullam imperdiet, arcu non tempor pulvinar, ipsum felis eleifend mi, a molestie eros libero at ligula. Morbi nec lorem quis metus dignissim cursus sit amet sit amet eros. Fusce tincidunt lorem a eleifend mattis. Sed eu luctus augue. Etiam sagittis sem vitae eros tempus, a suscipit metus faucibus. Etiam commodo venenatis ipsum et pellentesque. Maecenas elementum ultrices neque, eget mollis ipsum pulvinar et. Quisque euismod dui ipsum. Suspendisse egestas mi sed orci aliquet maximus.</p>\r\n<p>Ut vel vestibulum risus, at hendrerit nisl. Aenean tristique in massa eget iaculis. Aliquam ac feugiat nunc. Morbi pellentesque venenatis nisi, quis imperdiet leo congue et. Morbi tempus laoreet erat, id fringilla dolor finibus at. Ut non diam nec arcu efficitur dignissim. Morbi ac orci nunc. Sed lobortis ligula feugiat, fringilla lorem a, sagittis odio. Quisque cursus sapien velit, vitae aliquet ipsum luctus sed. Vestibulum quis sodales orci, a lobortis lorem. Cras quis orci a libero fringilla scelerisque. Curabitur ornare lorem purus, non tincidunt velit porta eu. Morbi suscipit nulla neque, blandit cursus risus dictum eget. Curabitur viverra convallis iaculis. Nunc venenatis est et diam mattis euismod.</p>\r\n<p>Integer vehicula, elit sed rutrum ultrices, ante dolor posuere turpis, ultricies commodo nibh ante eu justo. Sed feugiat hendrerit ligula, ut tincidunt lorem iaculis quis. Donec tristique purus leo, quis blandit odio iaculis vel. Sed nec magna non velit malesuada commodo vitae vel leo. Duis ut eros vel felis sodales dignissim. Donec pellentesque vel libero non eleifend. Sed ullamcorper, magna ultricies sagittis posuere, purus nibh placerat ante, nec mollis velit quam a felis.</p>\r\n<p>Aliquam erat volutpat. Donec vitae lacus quis nisi pharetra rutrum et sed sapien. Etiam augue massa, convallis a efficitur nec, porttitor at ligula. Nullam luctus elit id sapien elementum, a placerat quam porta. Curabitur mattis purus in fermentum consectetur. Curabitur interdum nulla ligula, non pulvinar magna molestie eu. Fusce id leo a velit faucibus suscipit. Donec tincidunt leo ac vehicula volutpat. Vestibulum leo tellus, laoreet vel efficitur eget, aliquet sit amet libero. Nulla facilisi. Duis malesuada consequat cursus.</p>', 1, 1487514022, 0, 0, 5, 4, '', '');
 
 -- --------------------------------------------------------
 
@@ -99,15 +99,38 @@ CREATE TABLE `bgdy_components` (
   `description` varchar(5000) DEFAULT NULL,
   `internal_name` varchar(255) DEFAULT NULL,
   `is_frontend` int(11) NOT NULL DEFAULT '0',
-  `is_backend` int(11) NOT NULL DEFAULT '0'
+  `is_backend` int(11) NOT NULL DEFAULT '0',
+  `is_locked` int(11) DEFAULT '0',
+  `author_name` varchar(500) DEFAULT NULL,
+  `author_url` varchar(2000) DEFAULT NULL,
+  `version` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `bgdy_components`
 --
 
-INSERT INTO `bgdy_components` (`id`, `title`, `description`, `internal_name`, `is_frontend`, `is_backend`) VALUES
-(3, 'Content', NULL, 'content', 1, 0);
+INSERT INTO `bgdy_components` (`id`, `title`, `description`, `internal_name`, `is_frontend`, `is_backend`, `is_locked`, `author_name`, `author_url`, `version`) VALUES
+(3, 'Content', NULL, 'content', 1, 0, 0, '', '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bgdy_components_modules`
+--
+
+CREATE TABLE `bgdy_components_modules` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `description` varchar(5000) DEFAULT NULL,
+  `internal_name` varchar(255) DEFAULT NULL,
+  `is_frontend` int(11) NOT NULL DEFAULT '0',
+  `is_backend` int(11) NOT NULL DEFAULT '0',
+  `is_locked` int(11) DEFAULT '0',
+  `author_name` varchar(500) DEFAULT NULL,
+  `author_url` varchar(2000) DEFAULT NULL,
+  `version` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -184,6 +207,13 @@ CREATE TABLE `bgdy_sessions` (
   `last_action_time` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `bgdy_sessions`
+--
+
+INSERT INTO `bgdy_sessions` (`id`, `php_session_id`, `user_id`, `last_action_time`) VALUES
+(1, 'q28r0ia3lp2lvnm16e9653r043', 5, 1501843899);
+
 -- --------------------------------------------------------
 
 --
@@ -196,6 +226,13 @@ CREATE TABLE `bgdy_usergroups` (
   `colour` varchar(255) DEFAULT NULL,
   `is_admin` int(11) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bgdy_usergroups`
+--
+
+INSERT INTO `bgdy_usergroups` (`id`, `title`, `colour`, `is_admin`) VALUES
+(1, 'Administrators', '#000000', 1);
 
 -- --------------------------------------------------------
 
@@ -223,7 +260,7 @@ CREATE TABLE `bgdy_users` (
 --
 
 INSERT INTO `bgdy_users` (`id`, `username`, `real_name`, `email`, `usergroup_id`, `password`, `activated`, `blocked`, `blocked_reason`, `register_time`, `last_action_time`, `verify_token`) VALUES
-(5, 'Smith0r', 'Chris Smith', 'smith0r54@gmail.com', 3, NULL, 1, 0, NULL, 1487513679, NULL, NULL);
+(5, 'Smith0r', 'Chris Smith', 'smith0r54@gmail.com', 1, '$2y$10$wXWSowM2xqTR.bxIgwDKoO0fq1GX8/iqDatJEJaL.v0NAtM902IU.', 1, 0, NULL, 1487513679, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -264,6 +301,12 @@ ALTER TABLE `bgdy_articles_comments`
 -- Indexes for table `bgdy_components`
 --
 ALTER TABLE `bgdy_components`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `bgdy_components_modules`
+--
+ALTER TABLE `bgdy_components_modules`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -333,6 +376,11 @@ ALTER TABLE `bgdy_articles_comments`
 ALTER TABLE `bgdy_components`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
+-- AUTO_INCREMENT for table `bgdy_components_modules`
+--
+ALTER TABLE `bgdy_components_modules`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `bgdy_menus`
 --
 ALTER TABLE `bgdy_menus`
@@ -351,12 +399,12 @@ ALTER TABLE `bgdy_modules`
 -- AUTO_INCREMENT for table `bgdy_sessions`
 --
 ALTER TABLE `bgdy_sessions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `bgdy_usergroups`
 --
 ALTER TABLE `bgdy_usergroups`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `bgdy_users`
 --
