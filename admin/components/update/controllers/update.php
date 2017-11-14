5ea22e15bbdf39b187c81847146cda0d848cc960
Chris Smith <smith0r54@gmail.com>
2017-11-14 16:16:21 +0000

    Start of update process

--- admin/components/update/controllers/update.php
+++ admin/components/update/controllers/update.php
@@ -12,6 +12,34 @@
             $this->core = $core;
         }
         
+        public function update()
+        {
+            $opts = [
+                'http' => [
+                        'method' => 'GET',
+                        'header' => [
+                                'User-Agent: PHP'
+                        ]
+                ]
+            ];
+            $context = stream_context_create($opts);
+            $web_version = json_decode(file_get_contents("https://api.github.com/repos/Smith0r/bulletin/releases", false, $context));
+            $web_version = $web_version["0"];
+            $file = file_put_contents(__DIR__ ."/../../../../update.zip", file_get_contents($web_version->assets["0"]->browser_download_url));
+            $zip = new ZipArchive;
+            $res = $zip->open(__DIR__ ."/../../../../update.zip");
+            if ($res == TRUE)
+            {
+                $zip->extractTo(__DIR__ ."/../../../../");
+                $zip->close();
+                echo "Zip File Extracted! CMS Updated!";
+            }
+            else
+            {
+                echo "Does the zip file exist? Can't find it.";
+            }
+        }
+        
     }
 
 ?>
--- admin/components/update/views/update.php
+++ admin/components/update/views/update.php
@@ -1,7 +1,17 @@
 <?php
 
     $version = simplexml_load_file(__DIR__ ."/../../../version.xml");
-    $web_version = simplexml_load_file("https://raw.githubusercontent.com/Smith0r/bulletin/master/admin/version.xml", null, LIBXML_NOCDATA);
+    $opts = [
+        'http' => [
+                'method' => 'GET',
+                'header' => [
+                        'User-Agent: PHP'
+                ]
+        ]
+    ];
+    $context = stream_context_create($opts);
+    $web_version = json_decode(file_get_contents("https://api.github.com/repos/Smith0r/bulletin/releases", false, $context));
+    $web_version = $web_version["0"];
 
 ?>
 <div class="card">
@@ -9,13 +19,13 @@
     <div class="action-bar">
         <a href="index.php?component=settings&controller=settings&extension=update"><i class="fa fa-cog"></i> Settings</a>
     </div>
-    <?php if (version_compare($version->numerical, $web_version->numerical)) { ?>
+    <?php if (version_compare($version->numerical, $web_version->tag_name)) { ?>
         <p>&nbsp;</p>
-        <h3>What's new in this version?</h3>
+        <h3>Release Information for <?php echo $web_version->name; ?></h3>
         <div class="new-version-notes">
-            <?php echo $web_version->new; ?>
+            <?php echo $web_version->body; ?>
         </div>
-        <p style="text-align: center;"><a href="" class="btn btn-success btn-lg">UPDATE TO VERSION <?php echo $version->numerical; ?></a></p>
+        <p style="text-align: center;"><a href="index.php?component=update&controller=update&task=update" class="btn btn-success btn-lg">UPDATE TO VERSION <?php echo $web_version->name; ?></a></p>
     <?php } else { ?>
         <p>&nbsp;</p>
         <p>Looks like your Bulletin installation (v<?php echo $version->numerical; ?>) is up to date. You don't need to do anything!</p>
