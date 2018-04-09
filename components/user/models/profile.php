6e4ed197beacf462719c6dd0cf214df509b53976
Chris Smith <smith0r54@gmail.com>
2018-04-09 12:36:49 +0100

    Recent articles on profile

--- components/user/models/profile.php
+++ components/user/models/profile.php
@@ -1,5 +1,7 @@
 <?php
 
+    require_once(__DIR__ ."/../../content/models/article.php");
+
     class ProfileModel extends Model
     {
         
@@ -7,11 +9,17 @@
         public $database;
         
         public $user;
+        public $articles = array();
         
         public function __construct($id = 0, $database)
         {
             $this->database = $database;
             $this->user = new UserModel($id, $this->database);
+            $recents = $this->database->loadObjectList("SELECT id FROM #__articles WHERE author_id = ? AND published = '1' ORDER BY publish_time DESC LIMIT 5", array($this->user->id));
+            foreach ($recents as $recent)
+            {
+                $this->articles[] = new ArticleModel($recent->id, $this->database);
+            }
         }
         
     }
--- components/user/views/profile.php
+++ components/user/views/profile.php
@@ -8,6 +8,15 @@
                 <li><strong>Member since:</strong> <?php echo date("jS F Y", $this->model->user->register_time); ?></li>
                 <li><strong>Last Online:</strong> <?php echo ((time() - $this->model->user->last_action_time) <= 900 ? "Online Now" : $this->model->relativeTime($this->model->user->last_action_time) ." ago"); ?></li>
             </ul>
+            <h3 style="margin-top: 40px;">Recent Articles by <?php echo $this->model->user->username; ?></h3>
+            <ul>
+                <?php foreach ($this->model->articles as $article) { ?>
+                    <li>
+                        <p><a href="<?php echo Core::route("index.php?component=content&controller=article&id=". $article->id); ?>"><?php echo $article->title; ?></a></p>
+                        <p><?php echo $this->model->relativeTime($article->publish_time); ?> ago</p>
+                    </li>
+                <?php } ?>
+            </ul>
         </div>
     </div>
 </div>
