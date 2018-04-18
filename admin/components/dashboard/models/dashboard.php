a9094efc455128e05e26a939e0d5aa4c97c4874b
Chris Smith <smith0r54@gmail.com>
2018-04-18 09:43:21 +0100

    Better Dashboard. Fixes #47

--- admin/components/dashboard/models/dashboard.php
+++ admin/components/dashboard/models/dashboard.php
@@ -1,5 +1,7 @@
 <?php
 
+    require_once(__DIR__ ."/../../content/models/article.php");
+
     class DashboardModel
     {
         
@@ -9,6 +11,8 @@
         public $article_count = 0;
         public $user_count = 0;
         public $launch_days = 0;
+        public $users = array();
+        public $articles = array();
         
         public function __construct($id = 0, $database)
         {
@@ -18,10 +22,20 @@
         
         public function load()
         {
-            $articles = $this->database->loadObject("SELECT COUNT(id) AS count FROM #__articles");
-            $this->article_count = $articles->count;
-            $users = $this->database->loadObject("SELECT COUNT(id) AS count FROM #__users");
-            $this->user_count = $users->count;
+            $t_articles = $this->database->loadObjectList("SELECT id FROM #__articles ORDER BY publish_time DESC LIMIT 5");
+            foreach ($t_articles as $t_article)
+            {
+                $article = new ArticleModel($t_article->id, $this->database);
+                $this->articles[] = $article;
+            }
+            $t_users = $this->database->loadObjectList("SELECT id FROM #__users ORDER BY register_time DESC LIMIT 5");
+            foreach ($t_users as $t_user)
+            {
+                $user = new UserModel($t_user->id, $this->database);
+                $this->users[] = $user;
+            }
+            $this->article_count = count($this->articles);
+            $this->user_count = count($this->users);
             $this->launch_days = number_format(((time() - LAUNCH_TIME) / 86400), 1);
         }
         
--- admin/components/dashboard/views/dashboard.php
+++ admin/components/dashboard/views/dashboard.php
@@ -1,6 +1,26 @@
-<div class="white-card">
-    <h2>Website Statistics</h2>
-    <p><strong><?php echo $this->model->article_count; ?></strong> Articles</p>
-    <p><strong><?php echo $this->model->user_count; ?></strong> Users</p>
-    <p><strong><?php echo $this->model->launch_days; ?></strong> Days since launch</p>
+<div class="row">
+    <div class="col-md-4">
+        <div class="white-card">
+            <h2>Website Statistics</h2>
+            <p><strong><?php echo $this->model->article_count; ?></strong> Articles</p>
+            <p><strong><?php echo $this->model->user_count; ?></strong> Users</p>
+            <p><strong><?php echo $this->model->launch_days; ?></strong> Days since launch</p>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="white-card">
+            <h2>Latest Articles</h2>
+            <?php foreach ($this->model->articles as $article) { ?>
+                <p><a href="index.php?component=content&controller=article&id=<?php echo $article->id; ?>"><?php echo $article->title; ?></a><br />By <?php echo $article->author->username; ?></p>
+            <?php } ?>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="white-card">
+            <h2>Latest Users</h2>
+            <?php foreach ($this->model->users as $user) { ?>
+                <p><a href="index.php?component=user&controller=user&id=<?php echo $user->id; ?>"><?php echo $user->username; ?></a><br /><?php echo date("jS F Y", $user->register_time); ?></p>
+            <?php } ?>
+        </div>
+    </div>
 </div>
