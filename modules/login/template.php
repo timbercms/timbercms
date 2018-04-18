f08a9cff7d99148058c99677de2301af09f098c3
Chris Smith <smith0r54@gmail.com>
2018-04-18 12:32:29 +0100

    Proper routing.

--- modules/login/template.php
+++ modules/login/template.php
@@ -8,12 +8,12 @@
                 <?php echo Core::user()->username; ?>
             </div>
             <ul class="login-options">
-                <li><a href="<?php echo $this->route("index.php?component=user&controller=profile&id=". Core::user()->id); ?>">My Profile</a></li>
-                <li><a href="<?php echo $this->route("index.php?component=user&controller=settings"); ?>">Settings</a></li>
+                <li><a href="<?php echo Core::route("index.php?component=user&controller=profile&id=". Core::user()->id); ?>">My Profile</a></li>
+                <li><a href="<?php echo Core::route("index.php?component=user&controller=settings"); ?>">Settings</a></li>
                 <?php if (Core::user()->usergroup->is_admin == 1) { ?>
                     <li><a href="<?php echo BASE_URL; ?>admin">Admin Panel</a></li>
                 <?php } ?>
-                <li><a href="<?php echo $this->route("index.php?component=user&controller=user&task=logout"); ?>">Logout</a></li>
+                <li><a href="<?php echo Core::route("index.php?component=user&controller=user&task=logout"); ?>">Logout</a></li>
             </ul>
         </div>
     <?php } else { ?>
@@ -29,7 +29,7 @@
                 <div class="col-md-9">
                     <input type="password" name="password" class="form-control" />
                     <p style="text-align: right;">
-                        <a href="<?php echo $this->route("index.php?component=user&controller=requestreset"); ?>">Forgotten password?</a>
+                        <a href="<?php echo Core::route("index.php?component=user&controller=requestreset"); ?>">Forgotten password?</a>
                     </p>
                 </div>
             </div>
