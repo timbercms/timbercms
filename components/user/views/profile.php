458a0c84c4e18e6928f482cd1ba0bd37fa4ee49c
Chris Smith <smith0r54@gmail.com>
2018-04-18 10:01:18 +0100

    Slightly better profile design. Fixes #49

--- components/user/views/profile.php
+++ components/user/views/profile.php
@@ -1,19 +1,25 @@
 <div class="row">
-    <div class="col-md-6">
+    <div class="col-md-4">
         <img src="https://www.gravatar.com/avatar/<?php echo md5(strtolower(trim($this->model->user->email))); ?>?s=200" class="user-profile-avatar" />
-        <h1><?php echo $this->model->user->real_name; ?> (<?php echo $this->model->user->username; ?>)</h1>
+    </div>
+    <div class="col-md-8">
         <div class="user-information">
-            <h3>About <?php echo explode(" ", $this->model->user->real_name)["0"]; ?></h3>
+            <h3>About <?php echo explode(" ", $this->model->user->username)["0"]; ?></h3>
             <ul>
                 <li><strong>Member since:</strong> <?php echo date("jS F Y", $this->model->user->register_time); ?></li>
                 <li><strong>Last Online:</strong> <?php echo ((time() - $this->model->user->last_action_time) <= 900 ? "Online Now" : $this->model->relativeTime($this->model->user->last_action_time) ." ago"); ?></li>
             </ul>
+        </div>
+    </div>
+</div>
+<div class="user-information">
+    <div class="row">
+        <div class="col-md-6">
             <h3 style="margin-top: 40px;">Recent Articles by <?php echo $this->model->user->username; ?></h3>
             <ul>
                 <?php foreach ($this->model->articles as $article) { ?>
                     <li>
-                        <p><a href="<?php echo Core::route("index.php?component=content&controller=article&id=". $article->id); ?>"><?php echo $article->title; ?></a></p>
-                        <p><?php echo $this->model->relativeTime($article->publish_time); ?> ago</p>
+                        <p><a href="<?php echo Core::route("index.php?component=content&controller=article&id=". $article->id); ?>"><?php echo $article->title; ?></a><br /><?php echo $this->model->relativeTime($article->publish_time); ?> ago</p>
                     </li>
                 <?php } ?>
             </ul>
