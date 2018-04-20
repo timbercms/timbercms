10c6926c628ef24d1d9484e5f1d433b4bfd37813
Chris Smith <smith0r54@gmail.com>
2018-04-20 10:59:29 +0100

    Fix Sitemap Hook

--- hooks/sitemap.php
+++ hooks/sitemap.php
@@ -13,16 +13,12 @@
         public function onXMLSitemap()
         {
             $time = date("Y-m-d", time());
-            $items = $this->database->loadObjectList("SELECT id FROM #__users WHERE activated = 1");
-            foreach ($items as $item)
-            {
-                echo '    <url>';
-                    echo '        <loc><![CDATA['. Core::route("index.php?component=sitemap&controller=sitemap") .']]></loc>';
-                    echo '        <lastmod>'. $time .'</lastmod>';
-                    echo '        <changefreq>weekly</changefreq>';
-                    echo '        <priority>0.5</priority>';
-                echo '    </url>';
-            }
+            echo '    <url>';
+                echo '        <loc><![CDATA['. Core::route("index.php?component=sitemap&controller=sitemap") .']]></loc>';
+                echo '        <lastmod>'. $time .'</lastmod>';
+                echo '        <changefreq>weekly</changefreq>';
+                echo '        <priority>0.5</priority>';
+            echo '    </url>';
         }
 	}
 
