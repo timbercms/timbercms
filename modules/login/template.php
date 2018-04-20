bd82a50046aafa68a8f4458691502a471d19dd4b
Chris Smith <smith0r54@gmail.com>
2018-04-20 10:05:09 +0100

    Frontend template colours. Fixes #75

--- modules/login/template.php
+++ modules/login/template.php
@@ -11,7 +11,7 @@
                 <li><a href="<?php echo Core::route("index.php?component=user&controller=profile&id=". Core::user()->id); ?>">My Profile</a></li>
                 <li><a href="<?php echo Core::route("index.php?component=user&controller=settings"); ?>">Settings</a></li>
                 <?php if (Core::user()->usergroup->is_admin == 1) { ?>
-                    <li><a href="<?php echo BASE_URL; ?>admin">Admin Panel</a></li>
+                    <li><a href="<?php echo BASE_URL; ?>admin" target="_blank">Admin Panel</a></li>
                 <?php } ?>
                 <li><a href="<?php echo Core::route("index.php?component=user&controller=user&task=logout"); ?>">Logout</a></li>
             </ul>
