7e0fe6ebc4814361d1e6b4c721497427ae656072
Chris Smith <smith0r54@gmail.com>
2018-05-18 11:57:42 +0100

    Register Captcha Fix

--- components/user/controllers/register.php
+++ components/user/controllers/register.php
@@ -5,6 +5,11 @@
         
         public function __construct()
         {
+            Core::changeTitle("Register an account");
+            if (Core::config()->enable_recaptcha == 1)
+            {
+                Core::addScript("https://www.google.com/recaptcha/api.js");
+            }
         }
         
     }
