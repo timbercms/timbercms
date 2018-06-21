c10fce36ee7aed62505992b69c96ab92d6827f60
Chris Smith <smith0r54@gmail.com>
2018-06-21 09:57:53 +0100

    Update Mobile_Detect

--- templates/default/index.php
+++ templates/default/index.php
@@ -9,7 +9,6 @@
         <!-- META_ITEMPROPS -->
         <!-- META_NAMES -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
-        <link rel="stylesheet" href="<?php echo BASE_URL; ?>templates/<?php echo $this->template->name; ?>/css/font-awesome.min.css">
         <link rel="stylesheet" href="<?php echo BASE_URL; ?>templates/<?php echo $this->template->name; ?>/css/bootstrap.min.css">
         <link rel="stylesheet" href="<?php echo BASE_URL; ?>templates/<?php echo $this->template->name; ?>/css/template.css?v=<?php echo time(); ?>">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
