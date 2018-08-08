0877f67f8002cd169cbe7f9902af7ffbe9512cdd
Chris Smith <smith0r54@gmail.com>
2018-08-08 11:28:31 +0100

    Fix MenuItem Bug. Fixes #150

--- admin/components/menu/views/newitem.php
+++ admin/components/menu/views/newitem.php
@@ -1,7 +1,7 @@
 <div class="white-card">
     <h2><?php echo (strlen($_GET["id"]) > 0 ? "Change" : "New"); ?> Menu Item</h2>
     <div class="component-action-bar">
-        <a href="index.php?component=menu&controller=menuitems&menu_id=<?php echo $_GET["menu_id"]; ?>" class="button"><i class="fa fa-chevron-left"></i> Back to List</a>
+        <a href="index.php?component=menu&controller=menuitems&id=<?php echo $_GET["menu_id"]; ?>" class="button"><i class="fa fa-chevron-left"></i> Back to List</a>
     </div>
     <div class="component-list">
         <?php foreach ($this->model->components as $key => $component) { ?>
