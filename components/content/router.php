592a94286855e3c2ffe64b6aef5ec287d809b2c5
Chris Smith <smith0r54@gmail.com>
2018-09-11 10:02:18 +0100

    Fix content router. Fixes #165

--- components/content/router.php
+++ components/content/router.php
@@ -40,42 +40,132 @@
                 if ($controller == "article" && $id > 0)
                 {
                     $article = new ArticleModel($id, $this->database);
-                    $item = Core::db()->loadObject("SELECT id, alias FROM #__menus_items WHERE component = ? AND controller = ? AND content_id = ? AND is_home != '1'", array($this->component, "category", $article->category->id));
-                    if ($item->id > 0)
+                    $category = $this->database->loadObject("SELECT id, parent_id, alias FROM #__articles_categories WHERE id = ?", array($article->category->id));
+                    if ($category->id > 0)
                     {
-                        return BASE_URL .$item->alias."/". $article->alias;
-                    }
-                    else
-                    {
-                        $url_parts = array($article->category->alias);
-                        if ($article->category->parent_id > 0)
+                        $url_parts[] = $article->alias;
+                        $url_parts[] = $category->alias;
+                        if ($category->parent_id > 0)
                         {
-                            $url_parts[] = $article->category->parent->alias;
-                            if ($article->category->parent->parent_id > 0)
+                            $parent = $this->database->loadObject("SELECT id, parent_id, alias FROM #__articles_categories WHERE id = ?", array($category->parent_id));
+                            if ($parent->id > 0)
                             {
-                                $cat = $this->database->loadObject("SELECT alias FROM #__articles_categories WHERE id = ?", array($article->category->parent->parent_id));
-                                $url_parts[] = $cat->alias;
+                                // Check to see if a menu item exists for the parent
+                                $parent_item = $this->database->loadObject("SELECT id, alias, parent_id FROM #__menus_items WHERE component = ? AND controller = ? AND content_id = ? AND is_home != '1'", array($this->component, "category", $parent->id));
+                                if ($parent_item->id > 0)
+                                {
+                                    // Menu item exists! Add alias and check for grandparent.
+                                    $url_parts[] = $parent->alias;
+                                    if ($parent_item->parent_id > 0)
+                                    {
+                                        $grandparent_item = $this->database->loadObject("SELECT id, alias, parent_id FROM #__menus_items WHERE id = ? AND is_home != '1'", array($parent_item->parent_id));
+                                        if ($grandparent_item->id > 0)
+                                        {
+                                            // Item found! Add the alias.
+                                            $url_parts[] = $grandparent_item->alias;
+                                        }
+                                    }
+                                }
+                            }
+                            $url_parts = implode("/", array_reverse($url_parts));
+                            return BASE_URL.$url_parts;
+                        }
+                        else
+                        {
+                            $item = Core::db()->loadObject("SELECT id, alias FROM #__menus_items WHERE component = ? AND controller = ? AND content_id = ? AND is_home != '1'", array($this->component, "category", $article->category->id));
+                            if ($item->id > 0)
+                            {
+                                return BASE_URL .$item->alias."/". $article->alias;
+                            }
+                            else
+                            {
+                                $url_parts = array($article->category->alias);
+                                if ($article->category->parent_id > 0)
+                                {
+                                    $url_parts[] = $article->category->parent->alias;
+                                    if ($article->category->parent->parent_id > 0)
+                                    {
+                                        $cat = $this->database->loadObject("SELECT alias FROM #__articles_categories WHERE id = ?", array($article->category->parent->parent_id));
+                                        $url_parts[] = $cat->alias;
+                                    }
+                                }
+                                $url_parts = implode("/", array_reverse($url_parts));
+                                return BASE_URL .$this->component ."/article/". $url_parts ."/". $article->alias;
                             }
                         }
-                        $url_parts = implode("/", array_reverse($url_parts));
-                        return BASE_URL .$this->component ."/article/". $url_parts ."/". $article->alias;
                     }
                 }
                 else if ($controller == "category" && $id > 0)
                 {
-                    $category = new CategoryModel($id, $this->database, false);
-                    $url_parts = array($category->alias);
-                    if ($category->parent_id > 0)
+                    // Check if this category, or it's parent, or it's grandparent has a menu item
+                    // If so, build the url in the direction of children. If a category has a menu item,
+                    // and has a parent, ignore the parent and use the current menu item.
+                    // From there, check if the category has any child categories.
+                    
+                    $url_parts = array();
+                    
+                    $item = $this->database->loadObject("SELECT id, alias, parent_id FROM #__menu_items WHERE component = ? AND controller = ? AND content_id = ? AND is_home != '1'", array($this->component, "category", $id));
+                    if ($item->id > 0)
                     {
-                        $url_parts[] = $category->parent->alias;
-                        if ($category->parent->parent_id > 0)
+                        // Menu item exists for this category! Use this alias.
+                        $url_parts[] = $item->alias;
+                        // Now check to see if this menu item has a parent.
+                        if ($item->parent_id > 0)
                         {
-                            $cat = $this->database->loadObject("SELECT alias FROM #__articles_categories WHERE id = ?", array($category->parent->parent_id));
-                            $url_parts[] = $cat->alias;
+                            $parent = $this->database->loadObject("SELECT id, alias, parent_id FROM #__menu_items WHERE component = ? AND controller = ? AND content_id = ? AND is_home != '1'", array($this->component, "category", $item->parent_id));
+                            if ($parent->id > 0)
+                            {
+                                $url_parts[] = $parent->alias;
+                                // Check to see if the grandparent exists.
+                                $grandparent = $this->database->loadObject("SELECT id, alias, parent_id FROM #__menu_items WHERE component = ? AND controller = ? AND content_id = ? AND is_home != '1'", array($this->component, "category", $parent->parent_id));
+                                if ($grandparent->id > 0)
+                                {
+                                    $url_parts[] = $grandparent->alias;
+                                }
+                            }
                         }
                     }
-                    $url_parts = implode("/", array_reverse($url_parts));
-                    return BASE_URL .$this->component ."/category/". $url_parts;
+                    else
+                    {
+                        // Check to see if this category has a parent, if so, check for a menu item
+                        $category = $this->database->loadObject("SELECT id, parent_id, alias FROM #__articles_categories WHERE id = ?", array($id));
+                        if ($category->id > 0)
+                        {
+                            $url_parts[] = $category->alias;
+                            if ($category->parent_id > 0)
+                            {
+                                $parent = $this->database->loadObject("SELECT id, parent_id, alias FROM #__articles_categories WHERE id = ?", array($category->parent_id));
+                                if ($parent->id > 0)
+                                {
+                                    // Check to see if a menu item exists for the parent
+                                    $parent_item = $this->database->loadObject("SELECT id, alias, parent_id FROM #__menus_items WHERE component = ? AND controller = ? AND content_id = ? AND is_home != '1'", array($this->component, "category", $parent->id));
+                                    if ($parent_item->id > 0)
+                                    {
+                                        // Menu item exists! Add alias and check for grandparent.
+                                        $url_parts[] = $parent->alias;
+                                        if ($parent_item->parent_id > 0)
+                                        {
+                                            $grandparent_item = $this->database->loadObject("SELECT id, alias, parent_id FROM #__menus_items WHERE id = ? AND is_home != '1'", array($parent_item->parent_id));
+                                            if ($grandparent_item->id > 0)
+                                            {
+                                                // Item found! Add the alias.
+                                                $url_parts[] = $grandparent_item->alias;
+                                            }
+                                        }
+                                    }
+                                }
+                                $url_parts = implode("/", array_reverse($url_parts));
+                                return BASE_URL.$url_parts;
+                            }
+                            else
+                            {
+                                // No parent, just use this alias.
+                                $url_parts[] = $category->alias;
+                                $url_parts = implode("/", $url_parts);
+                                return BASE_URL .$this->component ."/category/". $url_parts;
+                            }
+                        }
+                    }
                 }
                 else if ($controller == "search")
                 {
@@ -127,7 +217,7 @@
             }
             else
             {
-                // Menu Item Detection
+                // First, see if an article exists with the same alias, if not, check if a category exists.
                 $alias = end($parts);
                 $article = $this->database->loadObject("SELECT id FROM #__articles WHERE alias = ? AND published = 1", array($alias));
                 if ($article->id > 0)
@@ -136,8 +226,18 @@
                     $new_parts[] = "content";
                     $new_parts[] = "article";
                     $new_parts[] = $article->id;
+                    return $new_parts;
                 }
-                return $new_parts;
+                $category = $this->database->loadObject("SELECT id FROM #__articles_categories WHERE alias = ? AND published = 1", array($alias));
+                if ($category->id > 0)
+                {
+                    // Matched!
+                    $new_parts[] = "content";
+                    $new_parts[] = "category";
+                    $new_parts[] = $category->id;
+                    return $new_parts;
+                }
+                return false;
             }
             return false;
         }
