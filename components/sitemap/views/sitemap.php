246645a617cd325c6952c9aaf4da1d9bb4d35629
Chris Smith <smith0r54@gmail.com>
2018-10-08 16:59:46 +0100

    Fix sitemap. Fixes #203

--- components/sitemap/views/sitemap.php
+++ components/sitemap/views/sitemap.php
@@ -1,5 +1,7 @@
 <h1>Sitemap</h1>
 <?php foreach ($this->model->items as $item) { ?>
-    <p><a href="<?php echo Core::route("index.php?component=". $item->component ."&controller=". $item->controller .($item->content_id > 0 ? "&id=". $item->content_id : "")); ?>"><?php echo $item->title; ?></a></p>
+    <?php if ($item->component != "system") { ?>
+        <p><a href="<?php echo Core::route("index.php?component=". $item->component ."&controller=". $item->controller .($item->content_id > 0 ? "&id=". $item->content_id : "")); ?>"><?php echo $item->title; ?></a></p>
+    <?php } ?>
 <?php } ?>
 <?php Core::hooks()->executeHook("onHTMLSitemap"); ?>
