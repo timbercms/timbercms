5a9e9584c073d862782da9feb719ec86299cf37c
Chris Smith <smith0r54@gmail.com>
2018-10-11 15:31:03 +0100

    Small dashboard changes. Closes #176

--- admin/components/dashboard/views/dashboard.php
+++ admin/components/dashboard/views/dashboard.php
@@ -1,13 +1,5 @@
-<div class="row">
-    <div class="col-md-4">
-        <div class="white-card">
-            <h2>Website Statistics</h2>
-            <p><strong><?php echo $this->model->article_count; ?></strong> Articles</p>
-            <p><strong><?php echo $this->model->user_count; ?></strong> Users</p>
-            <p><strong><?php echo $this->model->launch_days; ?></strong> Days since launch</p>
-        </div>
-    </div>
-    <div class="col-md-4">
+<div class="row dashboard-row">
+    <div class="col-md-6">
         <div class="white-card">
             <h2>Latest Articles</h2>
             <?php foreach ($this->model->articles as $article) { ?>
@@ -15,12 +7,22 @@
             <?php } ?>
         </div>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-6">
         <div class="white-card">
             <h2>Latest Users</h2>
             <?php foreach ($this->model->users as $user) { ?>
                 <p><a href="index.php?component=user&controller=user&id=<?php echo $user->id; ?>"><?php echo $user->username; ?></a><br /><?php echo date("jS F Y", $user->register_time); ?></p>
             <?php } ?>
+        </div>
+    </div>
+</div>
+<div class="row dashboard-row">
+    <div class="col-md-12">
+        <div class="white-card">
+            <h2>Website Statistics</h2>
+            <p><strong><?php echo $this->model->article_count; ?></strong> Articles</p>
+            <p><strong><?php echo $this->model->user_count; ?></strong> Users</p>
+            <p><strong><?php echo $this->model->launch_days; ?></strong> Days since launch</p>
         </div>
     </div>
 </div>
